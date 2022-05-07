1f81ce4ba4a8fe3a9fe7cd2bcf76075662f20317
John Kim Querobines <querobinesj@gmail.com>
2022-05-07 08:18:53 +0800

    feat: get datas and event emitters

--- backend/contracts/MyEpicGame.sol
+++ backend/contracts/MyEpicGame.sol
@@ -37,6 +37,18 @@ contract MyEpicGame is ERC721 {
     // to store the owner of the NFT and reference it later.
     mapping(address => uint256) public nftHolders;
 
+    // Solidity Events
+    event CharacterNFTMinted(
+        address sender,
+        uint256 tokenId,
+        uint256 characterIndex
+    );
+    event AttackComplete(
+        address sender,
+        uint256 newBossHp,
+        uint256 newPlayerHp
+    );
+
     // BBEG
 
     struct BigBoss {
@@ -181,6 +193,9 @@ contract MyEpicGame is ERC721 {
 
         // Increment the tokenId for the next person that uses it.
         _tokenIds.increment();
+
+        // Emit an event to let user know the NFT was minted.
+        emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
     }
 
     function attackBoss() public {
@@ -209,8 +224,37 @@ contract MyEpicGame is ERC721 {
             player.hp = player.hp - bigBoss.attackDamage;
         }
 
-        // Console for ease.
-        console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
-        console.log("Boss attacked player. New player hp: %s\n", player.hp);
+        // Let the user know the boss was attacked.
+        emit AttackComplete(msg.sender, bigBoss.hp, player.hp);
+    }
+
+    function checkIfUserHasNFT()
+        public
+        view
+        returns (CharacterAttributes memory)
+    {
+        // Get the tokenId of the user's character NFT
+        uint256 userNftTokenId = nftHolders[msg.sender];
+        // If the user has a tokenId in the map, return their character.
+        if (userNftTokenId > 0) {
+            return nftHolderAttributes[userNftTokenId];
+        }
+        // Else, return an empty character.
+        else {
+            CharacterAttributes memory emptyStruct;
+            return emptyStruct;
+        }
+    }
+
+    function getAllDefaultCharacters()
+        public
+        view
+        returns (CharacterAttributes[] memory)
+    {
+        return defaultCharacters;
+    }
+
+    function getBigBoss() public view returns (BigBoss memory) {
+        return bigBoss;
     }
 }
